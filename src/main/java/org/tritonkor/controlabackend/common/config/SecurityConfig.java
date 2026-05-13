1faf98accbe3a6500748a07f0dce077e949ddfc0
TRITONKOR <sasalahotskij@gmail.com>
2026-05-13 19:22:12 +0300

    fix: fix cors

--- src/main/java/org/tritonkor/controlabackend/common/config/SecurityConfig.java
+++ src/main/java/org/tritonkor/controlabackend/common/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> {})
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**", "/api/public/**").permitAll()
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
